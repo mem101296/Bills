86921498991f437688ef66df62730f882f96358a
mem101296 <mem101296@protonmail.com>
2020-05-14 20:05:48 -0500

    Added a new part
    Added a place to select which option. Original option to calculate paycheck leftover and new option to calcute paycheck using net amount.

--- 2-week-finances.py
+++ 2-week-finances.py
@@ -1,43 +1,59 @@
-paycheck = input("Please enter your paycheck amount: ")
-leftover = input("Please enter your leftovers from last paycheck: ")
-paycheck = float(paycheck) + float(leftover)
 bill = 0
 i = 0
-billlist = [ ]
+billlist = []
 billlist_float = []
 lnumber = 0
 bill_total = 0
 item = 0
+choise = 0
+payTax = 0
+payTaxSub = 0
 
-print("\n Type your bills bellow.\n Please type \"Done\" to finish adding bills")
-while True:
-    if bill == "Done":
-        break
+print("\nWhat would you like to do?")
+print("\nYou can choose:")
+print("\n1: Calculate money letfover after bills")
+print("\n2: Calculate paycheck amount after taxes")
+choise = input("\n")
 
-    elif bill == "done":
-        break
+if choise == "1":
+    paycheck = input("\nPlease enter your paycheck amount: ")
+    leftover = input("Please enter your leftovers from last paycheck: ")
+    paycheck = float(paycheck) + float(leftover)
 
-    else:
-        bill = input(f"Please enter bill {i}: ")
-        i += 1
-        billlist.append(bill)
+    print("\n Type your bills bellow.\n Please type \"Done\" to finish adding bills")
+    while True:
+        if bill == "Done":
+            break
 
-del billlist[-1]
+        elif bill == "done":
+            break
 
+        else:
+            bill = input(f"Please enter bill {i}: ")
+            i += 1
+            billlist.append(bill)
 
-for item in billlist:
-    billlist_float.append(float(item))
+    del billlist[-1]
 
+    for item in billlist:
+        billlist_float.append(float(item))
 
+    for lnumber in range(0, len(billlist)):
+        bill_total = bill_total + billlist_float[lnumber]
 
-for lnumber in range(0, len(billlist)):
+    print("The total amount of bills for this pay week is ")
+    print(bill_total)
 
+    afterbillpay = float(paycheck) - float(bill_total)
 
-    bill_total = bill_total + billlist_float[lnumber]
-print("The total amount of bills for this pay week is ")
-print(bill_total)
+    print("The leftover amount is: ")
+    print(afterbillpay)
 
-afterbillpay = float(paycheck) - float(bill_total)
+elif choise == "2":
+    payTax = input("\nPlease input paycheck net amount ")
 
-print("The leftover amount is: ")
-print(afterbillpay)
+    payTax = float(payTax)
+    payTaxSub = payTax * 0.17
+    payTax = payTax - payTaxSub
+
+    print("Your paycheck after taxes will be:", payTax)
